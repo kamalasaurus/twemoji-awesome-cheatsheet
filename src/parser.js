4da5ac688c6703242c524cd16a869f53afac1eb2
kamalasaurus <kamalasaurus@gmail.com>
2016-08-17 14:18:42 +0100

    grouping element names by emoji

--- src/parser.js
+++ src/parser.js
@@ -13,34 +13,68 @@ var STYLES_OUTPUT = path.join('dist', 'styles.css');
 
 
 var awesomeCss = fs.readFileSync(TWEMOJI_NAME, 'utf8');
-var elements = awesomeCss
+
+// only get class names and codePoints from the css rule
+function stripExcess(rule) {
+  name = rule
+    .match(/[^\s]+/)[0]
+    .replace(/\./g, '');
+
+  codePoint = rule
+    .split('/')
+    .slice(-1)[0]
+    .split('.')[0];
+
+  return [name, codePoint];
+}
+
+// filter out the non-emoji declarations
+function filterModifiers(namePointPair) {
+  return !~[
+    'twa-lg',
+    'twa-2x',
+    'twa-3x',
+    'twa-4x',
+    'twa-5x'
+  ].indexOf(namePointPair[0]);
+}
+
+function groupByPoint(group, namePointPair) {
+  var name = namePointPair[0];
+  var point = namePointPair[1];
+  group[point] ? group[point].push(name) : group[point] = [name];
+  return group;
+}
+
+var elementGroups = awesomeCss
   .split('\n\n')
-  .map(function(rule) {
-    return rule.match(/[^\s]+/)[0]
-      .replace(/\./g, ''); // only get class names from the css rule
-  })
   .slice(1)
-  .filter(function(name) {
-    return !~[
-      'twa-lg',
-      'twa-2x',
-      'twa-3x',
-      'twa-4x',
-      'twa-5x'
-    ].indexOf(name); // filter out the non-emoji declarations
-  })
+  .map(stripExcess)
+  .filter(filterModifiers)
+  .reduce(groupByPoint, {});
+
+var elements = Object
+  .keys(elementGroups)
   .map(makeElement);
 
 var header = [
   '<!doctype html>',
   '<html>',
   '\t<head>',
+  '\t\t<meta charset="UTF-8">',
   '\t\t<title>Twemoji Awesome Cheatsheet! 😃🤑😂</title>',
   '\t\t<link rel="stylesheet" type="text/css" href="twemoji-awesome.css">',
   '\t\t<link rel="stylesheet" type="text/css" href="styles.css">',
   '\t</head>',
   '\t<body>',
   '\t\t<div class="container">',
+].join('\n');
+
+var subheader = [
+  '\t\t\t<div class="subheader>"',
+  '\t\t\t\t<h1>Twemoji Awesome Cheatsheet</h1>',
+  '\t\t\t\t<p>make sure to declare <meta charset="UTF-8"> in the head of your app if you\'re using the non-ascii classes like åland-flag</p>',
+  '\t\t\t</div>',
 ].join('\n');
 
 var footer = [
@@ -50,16 +84,16 @@ var footer = [
 ].join('\n');
 
 
-function makeElement(name) {
+function makeElement(names) {
   return [
     '\t\t\t<div class="element">',
-    '\t\t\t\t<i class="twa twa-2x ' + name + '"></i>',
-    '\t\t\t\t<span>' + name + '</span>',
+    '\t\t\t\t<i class="twa twa-2x ' + name[0] + '"></i>',
+    '\t\t\t\t<span>' + names.join(', ') + '</span>',
     '\t\t\t</div>',
   ].join('\n');
 }
 
-var html = header + '\n' + elements.join('\n') + '\n' + footer;
+var html = header + '\n' + subheader + '\n' + elements.join('\n') + '\n' + footer;
 
 fs.writeFileSync(HTML_NAME, html);
 fs.createReadStream(TWEMOJI_NAME).pipe(fs.createWriteStream(TWEMOJI_OUTPUT));
